06d9104a5a8e51455f217838f45a9b7599ccedda
Tushar Dahiya <tushardahiya202@gmail.com>
2024-01-30 04:54:40 +0530

    CORS issue fix

--- index.js
+++ index.js
@@ -5,15 +5,12 @@ const { Server } = require("socket.io");
 const cors = require("cors");
 const PORT = process.env.PORT || 8080;
 
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://websocket-single-chat.onrender.com"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+app.use(
+  cors({
+    origin: "https://websocket-single-chat.onrender.com",
+    methods: ["GET", "POST"],
+  })
+);
 
 const server = http.createServer(app);
 const io = new Server(server, {
