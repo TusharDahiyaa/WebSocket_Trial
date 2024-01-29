b154a4b156fef0371afd77fba7a68c8056bb428f
Tushar Dahiya <tushardahiya202@gmail.com>
2024-01-30 04:15:10 +0530

    Backend of first WebSocket App

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+const PORT = process.env.PORT || 8080;
+
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {},
+});
+
+io.on("connection", (socket) => {
+  console.log(`User ${socket.id} connected.`);
+
+  socket.on("sendMessage", (data) => {
+    socket.broadcast.emit("receivedMessage", data);
+  });
+
+  //   When a client disconnects, perform this
+  socket.on("disconnect", () => {
+    console.log(`User ${socket.id} has left the chat`);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
